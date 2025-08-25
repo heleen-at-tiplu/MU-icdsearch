e8d488c77cc15235fe45a7669b4338bea43bb454
Musta <mustafaheleen12@gmail.com>
2025-08-25 09:35:28 +0200

    Setup project and hello world with loguru

---
+++ main.py
@@ -0,0 +1,14 @@
+from loguru import logger
+
+def main():
+    logger.debug("Hello from Loguru!")
+
+if __name__ == "__main__":
+    main()
+
+
+    print("Hello from icdsearch!")
+
+
+if __name__ == "__main__":
+    main()
